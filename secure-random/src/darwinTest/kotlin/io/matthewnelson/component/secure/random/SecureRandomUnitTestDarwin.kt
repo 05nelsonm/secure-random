ba86d89560b342063be4281d1bf06c7ff497e954
Matthew Nelson <developer@matthewnelson.io>
2023-01-12 13:52:33 -0500

    Implement nextBytes for Darwin

---
+++ secure-random/src/darwinTest/kotlin/io/matthewnelson/component/secure/random/SecureRandomUnitTestDarwin.kt
@@ -0,0 +1,11 @@
+package io.matthewnelson.component.secure.random
+
+import kotlin.test.Test
+
+class SecureRandomUnitTestDarwin {
+
+    @Test
+    fun givenByteArray_whenNextBytes_thenIsFilledWithData() {
+        SecureRandomUnitTest.givenByteArray_whenNextBytes_thenIsFilledWithData()
+    }
+}
