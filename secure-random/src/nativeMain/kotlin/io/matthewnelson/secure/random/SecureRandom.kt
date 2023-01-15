f0472a107f064288e773aa085c1eab3c614f47f1
Matthew Nelson <developer@matthewnelson.io>
2023-01-15 15:21:09 -0500

    Remove unnecessary memScoped call

--- secure-random/src/nativeMain/kotlin/io/matthewnelson/secure/random/SecureRandom.kt
+++ secure-random/src/nativeMain/kotlin/io/matthewnelson/secure/random/SecureRandom.kt
@@ -17,7 +17,6 @@ package io.matthewnelson.secure.random
 
 import io.matthewnelson.secure.random.internal.commonNextBytesOf
 import io.matthewnelson.secure.random.internal.SecRandomDelegate
-import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.usePinned
 
 /**
@@ -29,7 +28,6 @@ public actual class SecureRandom {
 
     public actual constructor(): this(SecRandomDelegate.instance())
     internal constructor(delegate: SecRandomDelegate) { this.delegate = delegate }
-
 
     /**
      * Returns a [ByteArray] of size [count], filled with
@@ -51,9 +49,7 @@ public actual class SecureRandom {
     public actual fun nextBytesCopyTo(bytes: ByteArray?) {
         bytes.ifNotNullOrEmpty {
             usePinned { pinned ->
-                memScoped {
-                    delegate.nextBytesCopyTo(pinned, size)
-                }
+                delegate.nextBytesCopyTo(pinned, size)
             }
         }
     }
